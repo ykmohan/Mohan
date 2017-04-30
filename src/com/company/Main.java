4c8ef49064733ae9e8a88e1c3da6e30c4b2a5d3a
krishna mahidhar chiluku <krishna mahidhar chiluku>
2017-04-30 12:26:15 +0530

    scanned string and int

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Hello World\n");
+        Scanner in =new Scanner(System.in);
+        String name;
+        int age;
+//        name= in.next();
+        age= in.nextInt();
+        System.out.println(age);
     }
 }
