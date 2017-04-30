837719cd349c47844922e190906b6b56f9eefe67
YK Mohan <mohanyasarapu@gmail.com>
2017-04-30 12:40:41 +0530

    product of two numbers using scanner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Hello World\n");
-        Scanner in =new Scanner(System.in);
-        String name;
-        int age;
-//        name= in.next();
-        age= in.nextInt();
-        System.out.println(age);
+        int a,b;
+        Scanner in = new Scanner(System.in);
+        a= in.nextInt();
+        b= in.nextInt();
+        System.out.println("1st number is " + a);
+        System.out.println("2nd number is " + b);
+        System.out.println("the product of two numbers are " +a*b);
     }
 }
