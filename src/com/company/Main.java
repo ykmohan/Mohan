8d163e5a78fcb048e20233f573e2f18bd8a59f1f
YK Mohan <mohanyasarapu@gmail.com>
2017-05-05 14:31:53 +0530

    scanner example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
         int a,b;
-        Scanner in = new Scanner(System.in);
-        a= in.nextInt();
-        b= in.nextInt();
-        System.out.println("1st number is " + a);
-        System.out.println("2nd number is " + b);
-        System.out.println("the product of two numbers are " +a*b);
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+       System.out.println("the 1st number is " +a);
+       System.out.println("the 2nd number is " +b);
+
+        System.out.println("the numbers between 1st and 2nd number are");
+
+        for (int i = a+1; i < b; i++) {
+System.out.println(i);
+        }
+
+
+
     }
-}
+
+    }
+
+
+
+
